80c1c26411c31e5257b178de46c8f92de4333314
HUST-hxj <1226891191@qq.com>
2022-01-25 22:29:15 +0800

    Add some features

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # C-Repl
 
-A simple C-Repl by Honeta.
+You can use it like this:
+
+>`crepl> 12 + 3`
+>
+>`15`
+
+or like this:
+
+>`crepl> sqrt(4)`
+>
+>`2`
+
+or even like this:
+
+>`crepl> int fibonacci(int n) { return n <= 2 ? 1 : fibonacci(n - 1) + fibonacci(n - 2); }`
+>
+>`Get: int fibonacci(int n) { return n <= 2 ? 1 : fibonacci(n - 1) + fibonacci(n - 2); }`
+>
+>`fibonacci(6)`
+>
+>`8`
+
+It supports signed or unsigned integer(also short/long long) and float(also double) calculations currently.Enjoy it!
--- crepl.c
+++ crepl.c
@@ -3,44 +3,56 @@
 #include <unistd.h>
 #include <wait.h>
 #include <math.h>
+#include <stdlib.h>
+#include <signal.h>
 #include <dlfcn.h>
 
-int isFunction(char line[])
-{
-  if (line[0] == 'i' && line[1] == 'n' && line[2] == 't' && line[3] == ' ')
-    return 1;
-  return 0;
+static char line[4096];
+long long function_count, expression_count;
+
+void handler(int arg) {
+  for (long long i = 1; i <= function_count; ++i) {
+    char name_so[100];
+    sprintf(name_so, "libfunc%lld.so", i);
+    remove(name_so);
+  }  
+  for (long long i = 1; i <= expression_count; ++i) {
+    char name_so[100];
+    sprintf(name_so, "libexpr%lld.so", i);
+    remove(name_so);
+  } 
+  exit(0);
 }
 
-int main(int argc, char *argv[])
-{
-  static char line[4096];
-  int function_count = 0, expression_count = 0;
-  while (1)
-  {
+int isFunction(char line[]) {
+  int offset;
+  for (offset = 0; line[offset] == ' '; ++offset);
+  return strncmp(line + offset, "unsigned ", 9) == 0 
+      || strncmp(line + offset, "short ", 4) == 0 
+      || strncmp(line + offset, "int ", 4) == 0 
+      || strncmp(line + offset, "double ", 7) == 0 
+      || strncmp(line + offset, "long ", 5) == 0
+      || strncmp(line + offset, "float ", 6) == 0;
+}
+
+int main() {
+  signal(SIGINT, handler);
+  while (1) {
     printf("crepl> ");
     fflush(stdout);
-    if (!fgets(line, sizeof(line), stdin))
-    {
-      break;
-    }
-    //printf("Got %zu chars.\n", strlen(line)); // WTF?
-    if (isFunction(line))
-    {
+    if (!fgets(line, sizeof(line), stdin)) break;
+    if (isFunction(line)) {
       ++function_count;
       FILE *fp = fopen("func.c", "w");
       fprintf(fp, "%s", line);
       fclose(fp);
       char name_so[100];
-      sprintf(name_so, "libfunc%d.so", function_count);
+      sprintf(name_so, "libfunc%lld.so", function_count);
       pid_t pid = fork();
       if (fork() == 0)
         execlp("gcc", "gcc", "-fPIC", "-w", "-shared", "-o", name_so, "func.c", NULL);
-      else
-      {
-        int status;
-        waitpid(pid, &status, 0);
-      }
+      else 
+        waitpid(pid, NULL, 0);
       remove("func.c");
       char path_so[100];
       sprintf(path_so, "./%s", name_so);
@@ -49,35 +61,29 @@ int main(int argc, char *argv[])
         printf("Invalid syntax.\n");
       else
         printf("Get: %s", line);
-    }
-    else
-    {
+    } else {
       ++expression_count;
       char name_expr[100];
-      sprintf(name_expr, "__expr_wrapper_%d", expression_count);
+      sprintf(name_expr, "__expr_wrapper_%lld", expression_count);
       FILE *fp = fopen("expr.c", "w");
-      fprintf(fp, "int %s() {return %s;}", name_expr, line);
+      fprintf(fp, "double %s() {return %s;}", name_expr, line);
       fclose(fp);
       char name_so[100];
-      sprintf(name_so, "libexpr%d.so", expression_count);
+      sprintf(name_so, "libexpr%lld.so", expression_count);
       pid_t pid = fork();
       if (fork() == 0)
         execlp("gcc", "gcc", "-fPIC", "-w", "-shared", "-o", name_so, "expr.c", NULL);
       else
-      {
-        int status;
-        waitpid(pid, &status, 0);
-      }
+        waitpid(pid, NULL, 0);
       remove("expr.c");
       char path_so[100];
       sprintf(path_so, "./%s", name_so);
       void *handle = dlopen(path_so, RTLD_LAZY);
-      if (!handle)
+      if (!handle) {
         printf("Invalid syntax.\n");
-      else
-      {
-        int (*expr)() = dlsym(handle, name_expr);
-        printf("%d\n", expr());
+      } else {
+        double (*expr)() = dlsym(handle, name_expr);
+        printf("%g\n", expr());
         dlclose(handle);
       }
       remove(path_so);
